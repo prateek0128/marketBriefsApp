2f0711b5f10e864080a1c4f8740469355e4a2ad3
prateek0128 <rajput.prateek28@gmail.com>
2025-10-08 12:56:13 +0530

    Completly Wokring build

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,6 @@ module.exports = function (api) {
     presets: ["babel-preset-expo"],
     plugins: [
       // Reanimated plugin if you use it
-      "react-native-reanimated/plugin",
       // Add any other plugins here
     ],
   };
